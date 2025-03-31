d437364b99bbeced007b7d990040a9e4cc893ccf
jpizarro4 <jpizarro4@iespompeufabra.cat>
2025-03-31 13:45:23 +0200

    Update sketch.js

--- cara/cara5/sketch.js
+++ cara/cara5/sketch.js
@@ -6,7 +6,7 @@ function setup() {
   bgColor = color(200, 220, 255); // Color de fondo inicial
 
   mic = new p5.AudioIn();
-  userStartAudio().then(() => mic.start()); // Se solicita permiso para el micrófono
+  mic.start(); // Se solicita permiso para el micrófono
 }
 
 function draw() {
@@ -17,7 +17,7 @@ function draw() {
   let leftEyeSize = 30 + 10 * cos(frameCount * 0.1);
   let rightEyeSize = 30 + 10 * sin(frameCount * 0.1);
   let vol = mic.getLevel(); // Obtener volumen del micrófono
-  let h = map(vol, 0, 1, 0, 300);
+  let h = map(vol, 0, 1, 0, 100);
 
   let x = 200 * noise(0.1 * frameCount);
   let y = 200 * noise(0.1 * frameCount + 100);
@@ -26,7 +26,7 @@ function draw() {
   let yeye = map(mouseY, 0, 400, -10, +10);
 
   // Cara
-  fill(240, 200, 180);
+  fill(240, 200-y, 180+x);
   ellipse(faceX, faceY, 240, 280);
 
   // Ojos
@@ -35,7 +35,7 @@ function draw() {
   ellipse(faceX + 55, faceY - 50, 45, rightEyeSize + 5);
 
   // Pupilas
-  fill(102, 41, 16);
+  fill(102-y, 41+x, 16-x);
   ellipse(faceX - 55 + xeye, faceY - 50 + yeye, 22, leftEyeSize / 2);
   ellipse(faceX + 55 + xeye, faceY - 50 + yeye, 22, rightEyeSize / 2);
 
@@ -46,6 +46,22 @@ function draw() {
   // Boca
   fill(0, 0, 0);
   arc(faceX, faceY + 60, 140, 40 + h, 0, PI);
+  
+  //Ceja
+  noFill();
+  stroke(0);
+  strokeWeight(5);
+  arc(faceX -60, faceY -80, 50, 20 + h, PI, 0);
+  line(330 , faceY -80 + h/6, 380, faceY -75);
+  
+  //Sombrero
+  fill( 0, 0, 0)
+  line(faceX -100, faceY -120, faceX +100, faceY -120);
+  rect( 240, 40, 115, +40)
+  
+
+
+
 
   // Mosca
   fill(255);

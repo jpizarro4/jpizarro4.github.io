b80ef30b8f19867d58cd0032903bb00a827d503d
jpizarro4 <jpizarro4@iespompeufabra.cat>
2026-01-16 00:37:35 +0100

    Update questions.js

--- tecno/selectivitat/2022/questions.js
+++ tecno/selectivitat/2022/questions.js
@@ -2,7 +2,7 @@ const questions = [
     {
         type: "questions",
         category: "materials",
-        text: "Qüestió 1: Per a realitzar un assaig de Charpy s'utilitza un pèndol d'1 m de longitud que a l'extrem té una massa de 22 kg. S'usa una proveta amb una entalla en forma de U i una secció de 80 mm². A l'instant inicial, el pèndol està en posició horitzontal (la barra del pèndol és paraŀlela al terra), i després de xocar contra la proveta al punt més baix de la seva trajectòria, el seu extrem s'eleva 250 mm. Quanta energia s'ha absorbit en el xoc?",
+        text: "Per a realitzar un assaig de Charpy s'utilitza un pèndol d'1 m de longitud que a l'extrem té una massa de 22 kg. S'usa una proveta amb una entalla en forma de U i una secció de 80 mm². A l'instant inicial, el pèndol està en posició horitzontal (la barra del pèndol és paraŀlela al terra), i després de xocar contra la proveta al punt més baix de la seva trajectòria, el seu extrem s'eleva 250 mm. Quanta energia s'ha absorbit en el xoc?",
         options: [
             { text: "202,3 J", value: "a" },
             { text: "215,7 J", value: "b" },
@@ -16,7 +16,7 @@ const questions = [
     {
         type: "questions",
         category: "metrologia",
-        text: "Qüestió 2: Una resistència elèctrica normalitzada de 390 Ω el valor de la qual pot estar comprès entre 382,2 Ω i 397,8 Ω té una tolerància del:",
+        text: "Una resistència elèctrica normalitzada de 390 Ω el valor de la qual pot estar comprès entre 382,2 Ω i 397,8 Ω té una tolerància del:",
         options: [
             { text: "±1 %", value: "a" },
             { text: "±2 %", value: "b" },
@@ -29,7 +29,7 @@ const questions = [
     {
         type: "questions",
         category: "energia",
-        text: "Qüestió 3: Un automòbil emet 157,8 g de CO2 per cada kilòmetre recorregut en vies interurbanes. Setmanalment gasta un dipòsit de 60 L de gasoil en aquests recorreguts i té un consum mitjà de 5,6 L/(100 km). Quina és la petjada de carboni que deixarà a l'atmosfera en una setmana?",
+        text: "Un automòbil emet 157,8 g de CO2 per cada kilòmetre recorregut en vies interurbanes. Setmanalment gasta un dipòsit de 60 L de gasoil en aquests recorreguts i té un consum mitjà de 5,6 L/(100 km). Quina és la petjada de carboni que deixarà a l'atmosfera en una setmana?",
         options: [
             { text: "169,1 kg de CO2", value: "a" },
             { text: "169,1 tones de CO2", value: "b" },
@@ -42,7 +42,7 @@ const questions = [
     {
         type: "questions",
         category: "electrics",
-        text: "Qüestió 4: Amb quines unitats es pot expressar la potència elèctrica consumida en un instant determinat en una llar?",
+        text: "Amb quines unitats es pot expressar la potència elèctrica consumida en un instant determinat en una llar?",
         options: [
             { text: "kW/h", value: "a" },
             { text: "kW · h", value: "b" },
@@ -55,7 +55,7 @@ const questions = [
     {
         type: "questions",
         category: "maquines",
-        text: "Qüestió 5: Un tren d'engranatges es connecta entre els eixos d'un motor elèctric i d'una roda. Disposa de 4 rodes dentades de z1 = 14, z2 = 48, z3 = 16 i z4 = 25. Determineu la relació de transmissió ωroda/ωmotor.",
+        text: "Un tren d'engranatges es connecta entre els eixos d'un motor elèctric i d'una roda. Disposa de 4 rodes dentades de z1 = 14, z2 = 48, z3 = 16 i z4 = 25. Determineu la relació de transmissió ωroda/ωmotor.",
         options: [
             { text: "0,186 7", value: "a" },
             { text: "5,357", value: "b" },
